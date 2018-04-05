0f9c268ec685a2d89fecf99e07824538b90547b0
John Geliberte <geliberteofficials@gmail.com>
2018-04-05 08:53:48 +0800

    Change base url to localhost

--- codeigniter/application/config/config.php
+++ codeigniter/application/config/config.php
@@ -24,7 +24,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://pms/';
+// $config['base_url'] = 'http://pms/';
+$config['base_url'] = 'http://localhost/';
 
 /*
 |--------------------------------------------------------------------------
