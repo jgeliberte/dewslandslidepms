4f97897c5f35d60078c5c83c194cb873de78bbf6
John Geliberte <geliberteofficials@gmail.com>
2018-04-06 13:24:03 +0800

    initial commit for python

---
+++ python/src/pms.py
@@ -0,0 +1,27 @@
+import pmsModel as model
+
+def insertReport()
+
+
+def insertModule()
+
+
+def insertMetric()
+
+
+def insertTeam()
+
+
+def categorizeReport()
+
+
+def deleteModule()
+
+
+def deleteMetric()
+
+
+def deleteReport()
+
+
+if __name__ == "__main__":
