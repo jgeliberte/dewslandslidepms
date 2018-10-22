e50475d2df550cffe793ea38951163248b184e07
raybuensuceso <rayandrew.buensuceso>
2018-10-22 18:48:12 +0800

    - Updated JS file for analytics page

--- codeigniter/js/analysis_charts.js
+++ codeigniter/js/analysis_charts.js
@@ -1,99 +1,195 @@
 
-let BULLETIN_DATA = [];
-let EWI_DATA = [];
+let SERIES_ARR = [$("metric-selection").val()];
+let TEAM_METRICS = [];
 
 $(document).ready(() => {
-    getBulletinTimeliness();
-    plotTimeliness();
-    $("#selection-area").on("change", () => {
-        plotTimeliness();
+    getTimelinessInfo();
+    initializeTeamNameOnChange();
+    initializeModuleNameOnChange();
+    initializeMetricNameOnChange();
+
+    getTeamMetrics()
+    .done((team_metrics) => {
+        TEAM_METRICS = team_metrics;
+    })
+    .then(() => {
+        const teams = getAllTeamNames("team_name");
+        teams.forEach(({ team_name, team_id }) => {
+            $("#team_name").append(`<option value="${team_id}">${team_name}</option>`);
+        });
     });
+    getSeriesData($("#metric_name").val());
 });
+
+function getAllTeamNames (attr) {
+    const flags = {};
+    const teams = TEAM_METRICS.filter((entry, index, self) => {
+        if (flags[entry[attr]]) return false;
+        flags[entry[attr]] = true;
+        return true;
+    });
+
+    return teams;
+}
+
+function initializeTeamNameOnChange () {
+    $("#team_name").change(({ currentTarget: { value: team_id } }) => {
+        const $module_input = $("#module_name");
+        $module_input.find("option").not("[value='']").remove();
+        const module_names = getModuleNames(team_id);
+        module_names.forEach(({ module_id, module_name }) => {
+            $module_input.append(`<option value="${module_id}">${module_name}</option>`);
+        });
+    });
+}
+
+function initializeModuleNameOnChange () {
+    $("#module_name").change(({ currentTarget: { value: module_id } }) => {
+        const $metric_input = $("#metric_name");
+        $metric_input.find("option").not("[value='']").remove();
+        const metric_names = getMetricNames(module_id);
+        metric_names.forEach(({ metric_name }) => {
+            $metric_input.append(`<option value="${metric_name}">${metric_name}</option>`);
+        });
+    });
+}
+
+function initializeMetricNameOnChange () {
+    $("#metric_name").change(({ currentTarget: { value: metric } }) => {
+        const metric_name = metric === "" ? "" : metric;
+        getSeriesData(metric_name);
+    });
+}
+
+function getModuleNames (team_id) {
+    const flags = {};
+    const module_names = TEAM_METRICS.filter((entry, index, self) => {
+        const { team_id: team, module_name } = entry;
+        if (team === team_id) {
+            if (flags[module_name]) return false;
+            flags[module_name] = true;
+            return true;
+        }
+        return false;
+    });
+    return module_names;
+}
+
+function getMetricNames (module_id) {
+    const flags = {};
+    const metric_names = TEAM_METRICS.filter((entry, index, self) => {
+        const { metric_module_id: metric, metric_name, metric_type } = entry;
+        if (metric === module_id && metric_type == 3) {
+            if (flags[metric_name]) return false;
+            flags[metric_name] = true;
+            return true;
+        }
+        return false;
+    });
+    return metric_names;
+}
 
 function getTimelinessInfo () {
     return $.getJSON("/Analysis_charts/getTimelinessResult");
 }
 
-function getBulletinTimeliness () {
-    getTimelinessInfo().done((timeliness_array) => {
-        timeliness_array.forEach((timeliness) => {
-            timeliness[2] === 1 ? BULLETIN_DATA.push(timeliness) : EWI_DATA.push(timeliness);
+// function initializeSelectOnChange () {
+//     $("#metric-selection").change(({ currentTarget: { value: metric } }) => {
+//         const metric_name = metric === "all" ? "all" : metric;
+//         getSeriesData(metric_name);
+//     });
+// }
+
+function getTeamMetrics () {
+    return $.getJSON("/Analysis_charts/getTeamMetrics");
+}
+
+function getSeriesData (metric_name) {
+    SERIES_ARR = [];
+    getTimelinessInfo().done((data) => {
+        Object.entries(data[0]).forEach((timeliness_data) => {
+            if (metric_name === timeliness_data[0]) {
+                console.log(timeliness_data[0]);
+                const series_data = {
+                    name: timeliness_data[0].toUpperCase(),
+                    data: timeliness_data[1]
+                };
+                SERIES_ARR.push(series_data);
+            } else if (metric_name === "") {
+                const series_data_all = {
+                    name: timeliness_data[0].toUpperCase(),
+                    data: timeliness_data[1]
+                };
+                SERIES_ARR.push(series_data_all);
+            }
         });
+        plotTimeliness(SERIES_ARR);
     });
 }
 
-function plotTimeliness () {
-    getTimelinessInfo().done((timeliness_data) => {
-        let INFORMATION = "";
-        $("#selection-area").val() === "bulletin" ? INFORMATION = BULLETIN_DATA : INFORMATION = EWI_DATA;
-
-        Highcharts.chart("chart-container", {
-            chart: {
-                type: "scatter",
-                zoomType: "xy"
-            },
+function plotTimeliness (series_array) {
+    Highcharts.chart("chart-container", {
+        chart: {
+            type: "scatter",
+            zoomType: "xy"
+        },
+        title: {
+            text: "Timeliness Chart"
+        },
+        subtitle: {
+            text: "Bulletin and EWI"
+        },
+        xAxis: {
             title: {
-                text: "Timeliness Chart"
+                enabled: true,
+                text: "Date Received"
             },
-            subtitle: {
-                text: "Bulletin and EWI"
+            labels: {
+                format: "{value:%Y-%b-%e}"
             },
-            xAxis: {
-                title: {
-                    enabled: true,
-                    text: "Date Received"
-                },
-                labels: {
-                    format: "{value:%Y-%b-%e}"
-                },
-                startOnTick: true,
-                endOnTick: true,
-                showLastLabel: true
-            },
-            yAxis: {
-                title: {
-                    text: "Execution Time"
-                }
-            },
-            // legend: {
-            //     layout: "vertical",
-            //     align: "left",
-            //     verticalAlign: "top",
-            //     x: 500,
-            //     y: 70,
-            //     floating: true,
-            //     backgroundColor: "#FFFFFF",
-            //     borderWidth: 1
-            // },
-            plotOptions: {
-                scatter: {
-                    marker: {
-                        radius: 5,
-                        states: {
-                            hover: {
-                                enabled: true,
-                                lineColor: "rgb(100,100,100)"
-                            }
-                        }
-                    },
+            startOnTick: true,
+            endOnTick: true,
+            showLastLabel: true
+        },
+        yAxis: {
+            title: {
+                text: "Execution Time"
+            }
+        },
+        // legend: {
+        //     layout: "vertical",
+        //     align: "left",
+        //     verticalAlign: "top",
+        //     x: 500,
+        //     y: 70,
+        //     floating: true,
+        //     backgroundColor: "#FFFFFF",
+        //     borderWidth: 1
+        // },
+        plotOptions: {
+            scatter: {
+                marker: {
+                    radius: 5,
                     states: {
                         hover: {
-                            marker: {
-                                enabled: false
-                            }
+                            enabled: true,
+                            lineColor: "rgb(100,100,100)"
                         }
-                    },
-                    tooltip: {
-                        headerFormat: "<b>{series.name}</b><br>",
-                        pointFormat: "{point.x:%Y-%b-%e}, {point.y} minutes"
                     }
+                },
+                states: {
+                    hover: {
+                        marker: {
+                            enabled: false
+                        }
+                    }
+                },
+                tooltip: {
+                    headerFormat: "<b>{series.name}</b><br>",
+                    pointFormat: "Received: {point.x:%Y-%b-%e}<br>Execution Time: {point.y}"
                 }
-            },
-            series: [{
-                name: "Bulletin",
-                color: "rgba(16, 46, 80, 0.3)",
-                data: INFORMATION
-            }]
-        });
+            }
+        },
+        series: series_array
     });
 }
