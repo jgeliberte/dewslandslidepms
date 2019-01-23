47e0df11a301218d1647c2fbadfa01def074b719
ivyjean19 <diligivyjean@gmail.com>
2019-01-23 10:55:06 +0800

    - upadate to the right path

--- network/network_checker.sh
+++ network/network_checker.sh
@@ -1,9 +1,10 @@
-sudo screen -S ip_50   python3 ~/ivy/dewslandslidepms/network.py 192.168.150.50
-sudo screen -S ip_71   python3 ~/ivy/dewslandslidepms/network.py 192.168.150.71
-sudo screen -S ip_75   python3 ~/ivy/dewslandslidepms/network.py 192.168.150.75
-sudo screen -S ip_77   python3 ~/ivy/dewslandslidepms/network.py 192.168.150.77
-sudo screen -S ip_78   python3 ~/ivy/dewslandslidepms/network.py 192.168.150.78
-sudo screen -S ip_91   python3 ~/ivy/dewslandslidepms/network.py 192.168.150.91
-sudo screen -S ip_92   python3 ~/ivy/dewslandslidepms/network.py 192.168.150.92
-sudo screen -S ip_93   python3 ~/ivy/dewslandslidepms/network.py 192.168.150.93
-sudo screen -S ip_google   python3 ~/ivy/dewslandslidepms/network.py www.google.com
+#!/bin/bash    
+sudo screen -S ip_50   python3 ~/ivy/dewslandslidepms/network/network_checkerr.py 192.168.150.50
+sudo screen -S ip_71   python3 ~/ivy/dewslandslidepms/network/network_checkerr.py 192.168.150.71
+sudo screen -S ip_75   python3 ~/ivy/dewslandslidepms/network/network_checkerr.py 192.168.150.75
+sudo screen -S ip_77   python3 ~/ivy/dewslandslidepms/network/network_checkerr.py 192.168.150.77
+sudo screen -S ip_78   python3 ~/ivy/dewslandslidepms/network/network_checkerr.py 192.168.150.78
+sudo screen -S ip_91   python3 ~/ivy/dewslandslidepms/network/network_checkerr.py 192.168.150.91
+sudo screen -S ip_92   python3 ~/ivy/dewslandslidepms/network/network_checkerr.py 192.168.150.92
+sudo screen -S ip_93   python3 ~/ivy/dewslandslidepms/network/network_checkerr.py 192.168.150.93
+sudo screen -S ip_google   python3 ~/ivy/dewslandslidepms/network/network_checkerr.py www.google.com
