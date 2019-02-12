a1cedb7d564d64705098555b1c051c1241710b07
ivyjean19 <diligivyjean@gmail.com>
2019-02-12 11:16:10 +0800

    - updated script_checker.py

--- python/src/network/script_checker.py
+++ python/src/network/script_checker.py
@@ -1,6 +1,5 @@
-#!/usr/bin/python
 import subprocess
-import sys
+
 ip_list = ["192.168.150.50", 
            "192.168.150.71", 
            "192.168.150.75", 
@@ -13,26 +12,38 @@ ip_list = ["192.168.150.50",
 
 while True:
     all_restarted = []
+    alive = []
     for ip in  ip_list:
         command = ip
         ip_id = "ip_"+command[-2:]
         if ip_id == "ip_om":
             ip_id = "google"
-        try:
-            p = subprocess.check_output(["screen -ls %s"%ip_id], universal_newlines=True, shell=True)
-            p = p.splitlines()[1:][:-1]
-            if len(p) > 1:
-                for id in p:
-                    pid = id.split(".")[0].replace("\t","")
-                    print ("screen -R %s -X quit"%pid)
-                    subprocess.run(["screen -R %s -X quit"%pid],shell=True)            
-        except subprocess.CalledProcessError:
+
+        
+        p = subprocess.Popen("screen -ls %s"%ip_id, shell=True, stdout=subprocess.PIPE).stdout.read()
+        p = p.splitlines()[1:][:-1]
+        
+        if len(p)== 0 :
             all_restarted.append(command)
             subprocess.run(["screen -dmS %s sudo python3 /var/www/html/python/src/network/network_checker.py %s" %(ip_id,command)], shell=True)
+        else:
+            alive.append(command)
+            # if len(p) > 1:
+            #      for id in p:
+            #          pid = id.split(".")[0].replace("\t","")
+            #          print ("screen -R %s -X quit"%pid)
+            #          subprocess.run(["screen -R %s -X quit"%pid],shell=True)            
+    
+            
+    print ("All restarted monitoring ==> "),
+    print (all_restarted)
+    print ("Alive ==> "),
+    print (alive)
 
-    if all_restarted == 0:
-        print ("All Restarted ip")
-        print (all_restarted)
+    if len(all_restarted) == 0 :
+        print (" All network is alive ")
+        print ("---------")
     else:
-        print ("All network is alive")
-        
+        print (" All restarted network ")
+        print (all_restarted)
+        print ("---------")
